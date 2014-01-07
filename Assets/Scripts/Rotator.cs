2821c538f563d1dc6227baed3bc47987e59847f3
jepaivinprivate <johannes.paivinen@iki.fi>
2014-01-07 10:04:23 +0200

    Rotationspeed is now configurable

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
 using System.Collections;
 
-public class Rotator : MonoBehaviour {
+public class Rotator : MonoBehaviour
+{
+	public float RotationSpeed = 1.0f;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
-		this.transform.rotation *=  Quaternion.AngleAxis(1, Vector3.one);
-		}
+	void Update ()
+	{	
+		this.transform.rotation *=  Quaternion.AngleAxis(RotationSpeed, Vector3.one);
+	}
 }
